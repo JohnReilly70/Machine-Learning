8ef30cfc238ea5315cc1941ecbe2f24ae0be96b1
SgtSafety <johnreilly1989@hotmail.co.uk>
2017-03-14 17:08:55 +0000

    Part 4 Complete

--- machine_learning1.py
+++ machine_learning1.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import quandl
 import math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 quandl.ApiConfig.api_key = 'qw_5d_hjZhc8dsmyuB4x'
 
 df = quandl.get("WIKI/GOOGL")
 df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
-df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
-df['PCT_Change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0  # HIGH LOW PERCENTAGE
+df['PCT_Change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0 # PERCENTAGE CHANGE
 
 
 df = df[['Adj. Close','HL_PCT','PCT_Change','Adj. Volume']]
@@ -16,8 +19,22 @@ forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
 forecast_out = int(math.ceil(0.01*len(df)))
+print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
-print(df.head())
-print(df.tail())
+
+X = np.array(df.drop(['label'],1))
+y = np.array(df['label'])
+X = preprocessing.scale(X)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.2)
+
+# Can change algorithm simply by using svm.SVR(), comment out which algorithm you don't want to use
+clf = LinearRegression(n_jobs=-1)
+# clf = svm.SVR(kernel='poly')
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
