a1f49c4e4ccdd2ae7795ce61a2e4dee57a7ba69e
SgtSafety <johnreilly1989@hotmail.co.uk>
2017-03-15 16:57:39 +0000

    Part 5 Complete

--- machine_learning1.py
+++ machine_learning1.py
@@ -4,6 +4,11 @@ import math
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 quandl.ApiConfig.api_key = 'qw_5d_hjZhc8dsmyuB4x'
 
@@ -22,19 +27,44 @@ forecast_out = int(math.ceil(0.01*len(df)))
 print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
 X = np.array(df.drop(['label'],1))
-y = np.array(df['label'])
 X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+df.dropna(inplace=True)
+
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.2)
 
 # Can change algorithm simply by using svm.SVR(), comment out which algorithm you don't want to use
-clf = LinearRegression(n_jobs=-1)
+clf = LinearRegression(n_jobs=-1) #can run more jobs at once
 # clf = svm.SVR(kernel='poly')
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
+# print(accuracy)
 
-print(accuracy)
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+print(df.tail(40))
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
